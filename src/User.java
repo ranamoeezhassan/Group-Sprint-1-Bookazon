417a8b2d3b287754af6098baf9a63380b057861a
Tyshawn Rene <tyshawnrene@gmail.com>
2025-09-28 23:23:19 -0400

    Created abstract class for addresses. Created Shipping and Billing classes that extend address. Changed behavior in User and Order to reflect address changes.

--- src/User.java
+++ src/User.java
@@ -6,24 +6,17 @@ public class User {
     private String subscription;
     private Cart cart;
     private ArrayList<Order> orders;
-    private String shippingAddressLine1;
-    private String shippingAddressLine2;
-    private String shippingAddressCity;
-    private String shippingAddressState;
-    private String shippingAddressZip;
-    private String shippingAddressCountry;
-    private String billingAddressLine1;
-    private String billingAddressLine2;
-    private String billingAddressCity;
-    private String billingAddressState;
-    private String billingAddressZip;
-    private String billingAddressCountry;
+    private ShippingAddress shippingAddress;
+    private BillingAddress billingAddress;
+    
 
     public User(String name, String subscription) {
         this.name = name;
         this.subscription = subscription;  // normal, gold, platinum, silver membership
         this.cart = new Cart();
         this.orders = new ArrayList<>();
+        this.shippingAddress = new ShippingAddress("", "", "", "", "", "");
+        this.billingAddress = new BillingAddress("", "", "", "", "", "");
     }
 
     public String getName() {
@@ -42,23 +35,24 @@ public class User {
         cart.viewCartDetails();
     }
 
-    public void setShippingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.shippingAddressLine1 = line1;
-        this.shippingAddressLine2 = line2;
-        this.shippingAddressCity = city;
-        this.shippingAddressState = state;
-        this.shippingAddressZip = zip;
-        this.shippingAddressCountry = country;
+    public void setShippingAddress(String line1, String line2, String city, String state, String zip, String country){
+        shippingAddress.setLine1(line1);
+        shippingAddress.setLine2(line2);
+        shippingAddress.setCity(city);
+        shippingAddress.setState(state);
+        shippingAddress.setZip(zip);
+        shippingAddress.setCountry(country);
     }
 
-    public void setBillingAddress(String line1, String line2, String city, String state, String zip, String country) {
-        this.billingAddressLine1 = line1;
-        this.billingAddressLine2 = line2;
-        this.billingAddressCity = city;
-        this.billingAddressState = state;
-        this.billingAddressZip = zip;
-        this.billingAddressCountry = country;
+    public void setBillingAddress(String line1, String line2, String city, String state, String zip, String country){
+        billingAddress.setLine1(line1);
+        billingAddress.setLine2(line2);
+        billingAddress.setCity(city);
+        billingAddress.setState(state);
+        billingAddress.setZip(zip);
+        billingAddress.setCountry(country);
     }
+
 
     public void addToCart(Book book, int quantity) {
         cart.addItem(new CartItem(book.getTitle(), book.getPrice(), quantity));
@@ -80,9 +74,7 @@ public class User {
     }
 
     public void checkout() {
-        Order order = new Order(cart, this.subscription);
-        order.setShippingAddress("123 Main St", "", "Springfield", "IL", "62701", "USA");
-        order.setBillingAddress("123 Main St", "", "Springfield", "IL", "62701", "USA");
+        Order order = new Order(cart, this.subscription, shippingAddress, billingAddress);
         order.setOrderStatus("Order Placed");
         order.setDateCreated("2024-01-01");
         order.setUserName(this.name);
